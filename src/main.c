e551bd9b595bfdde280b04078f0a27b3d8dbc9c0
Dindonich <dindonich@pop-os.localdomain>
2020-10-17 00:56:09 +0700

    first version of main, here test only ls, del and mv

---
+++ src/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "SMFS.h"
+#include <string.h> //для работы со строками
+
+int main(int argc, char const *argv[])
+{
+	if(argc < 2) 
+	{
+	    printf("missing arguments\n");
+	    return 0;
+	}
+
+	if(!strcmp(argv[1], "--move"))
+	{
+		if(argc < 4) 
+		{
+        	fprintf(stderr, "missing arguments to 'move'\n");
+        	return 0;
+    	}
+
+    	move(argv[2], argv[3]);
+
+	} else if (!strcmp(argv[1], "--ls")) {
+    	if (argc < 3) 
+    	{
+        	argv[2] = "./";
+    	}
+
+    	ls(argv[2]);
+
+    } else if (!strcmp(argv[1], "--del")) {
+    	if (argc < 3) 
+    	{
+        	fprintf(stderr, "missing arguments to 'del'\n");
+        	return 0;
+    	}
+
+    	del(argv[2]);
+    }
+	return 0;
+}
