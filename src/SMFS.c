6b701f87472d34fbb8b3612953531bb3d07f5938
Dindonich <dindonich@pop-os.localdomain>
2020-10-17 01:10:37 +0700

    this codestyle ohhh

--- src/SMFS.c
+++ src/SMFS.c
@@ -33,18 +33,18 @@ int ls (char *dir)
 	{
 		perror(dir);
 		return 0;
-    }
+	}
 
-    struct dirent *ep;
-    char newdir[512];
-    printf(BLUE "\n%s :\n" WHITE, dir);
+	struct dirent *ep;
+	char newdir[512];
+	printf(BLUE "\n%s :\n" WHITE, dir);
 
-    while((ep = readdir(thisdir)))
-    {
+	while((ep = readdir(thisdir)))
+	{
 		if(strncmp(ep->d_name, ".", 1))
 		{
 			printf(GREEN "\t%s\n" WHITE, ep->d_name);
-    	}
+		}
 	}
 
     closedir(thisdir);
