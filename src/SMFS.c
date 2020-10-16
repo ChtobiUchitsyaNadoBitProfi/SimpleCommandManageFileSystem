f547c9df94bfe915d153701042e6374559cb7de0
Dindonich <dindonich@pop-os.localdomain>
2020-10-17 00:47:28 +0700

    add ls(show files) function

--- src/SMFS.c
+++ src/SMFS.c
@@ -25,3 +25,28 @@ int	del(char *filename)
 	}
 	return 0;
 }
+
+int ls (char *dir)
+{
+	DIR *thisdir = opendir(dir);
+    if(!thisdir) 
+    {
+        perror(dir);
+        return 0;
+    }
+
+    struct dirent *ep;
+    char newdir[512];
+    printf(BLUE "\n%s :\n" WHITE, dir);
+
+    while((ep = readdir(thisdir)))
+    {
+        if(strncmp(ep->d_name, ".", 1))
+        {
+            printf(GREEN "\t%s\n" WHITE, ep->d_name);
+        }
+    }
+
+    closedir(thisdir);
+    return 0;
+}
